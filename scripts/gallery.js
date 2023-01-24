284ef4f5ba9b0e88446ec9a1b7b2585f967377c9
Tealful Eyes <contact@tealfuleyes.com>
2023-01-24 15:07:37 -0500

    Added sample gallery of 6 image placeholders. Added script to open and close lightbox to display full images ofthe ones selected on the gallery page. Corrected issue with the responsive menu hyperlinking the logo area and added more commentary in files

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,1 +1,36 @@
-/* LIGHTBOX GALLERY STUFF WILL GO IN HERE */
+// OPENS LIGHTBOX
+function openLightbox() {
+  document.getElementById("theLightbox").style.display = "block";
+}
+// OPENS LIGHTBOX
+function closeLightbox() {
+  document.getElementById("theLightbox").style.display = "none";
+}
+
+var imageNumber = 1;
+showLightbox(imageNumber);
+
+// THUMBNAIL IMAGE CONTROLS
+function currentSlide(n) {
+  showLightbox(imageNumber = n);
+}
+// BUTTON CONTROLS FOR LIGHTBOX FUNCTION
+function slideButtons(n) {
+  showLightbox(imageNumber += n);
+}
+
+// THIS OPERATES THE LIGHTBOX ELEMENTS ONCE ITS OPEN
+function showLightbox(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  var cap = document.getElementsByClassName("image");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {imageNumber = 1}
+  if (n < 1) {imageNumber = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[imageNumber-1].style.display = "block";
+  captionText.innerHTML = cap[imageNumber-1].alt;
+}
--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,5 +1,5 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function menuToggle() {
   var x = document.getElementById("navlinks");
   if (x.style.display === "block") {
     x.style.display = "none";
